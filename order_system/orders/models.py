8a92b6f364c4f30fd240309dd85983be625e7320
vavil <vavilonskiy99@mail.ru>
2025-02-24 15:56:12 +0500

    create orders model

--- order_system/orders/models.py
+++ order_system/orders/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import validate_email
 
-# Create your models here.
+class Orders(models.Model):
+    status_list = {
+        'created': 'created',
+        'processing': 'created',
+        'completed': 'created',
+        'cancelled': 'created'
+    }
+
+    product_name = models.CharField(max_length=30, blank=False, null=False, verbose_name='Наименование продукта')
+    quantity = models.PositiveSmallIntegerField(default=1, verbose_name='Количество')
+    customer_email = models.EmailField(max_length=255, validators=[validate_email], verbose_name='Электронная почта')
+    status = models.CharField(default='created', choices=status_list, verbose_name='Статус заказа')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время создания')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Дата и время обновления')
+
+    class Meta:
+        verbose_name='Заказ'
+        verbose_name_plural='Заказ'
+
+    def __str__(self):
+        return f'Order id: {self.pk}, product: {self.product_name}'
